78802b0b361cf0537d5cac956368bbfe3f4cb7a6
Bradley_Carson <bmcarson1671@gmail.com>
2017-08-30 10:51:19 -0400

    Cleaned all bios to empty strings

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const models = require('./models');
 
+// FINDING AND UPDATING ENTRIES
+
+models.User.findAll().then((results) => {
+  results.forEach((result) => {
+    // *** GOOD LOOKING PRINTING ***
+    // console.log("================");
+    // console.log(result.name);
+    // console.log(result.email);
+    // console.log(result.bio);
+
+    result.bio = '';
+    result.save();
+  })
+});
+
+
+
+
+//ADDED USERS TO DATABASE
+
 // models.User.create({
 //   name: 'Babadook',
 //   email: 'spooky@scary.org',
