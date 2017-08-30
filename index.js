16b13cd5c766ea56c567361c177931919d8520fb
Bradley_Carson <bmcarson1671@gmail.com>
2017-08-30 10:39:23 -0400

    added users to db

---
+++ index.js
@@ -0,0 +1,13 @@
+const models = require('./models');
+
+// models.User.create({
+//   name: 'Babadook',
+//   email: 'spooky@scary.org',
+//   bio: 'gimmme da book'
+// });
+//
+// models.User.create({
+//   name: 'vegeta',
+//   email: 'saiyanprince@google.com',
+//   bio: 'KAKAROT!!!'
+// });
