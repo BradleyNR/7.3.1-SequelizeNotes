4ba12f858fd40b323f750c893caf250f0b8bd462
Bradley_Carson <bmcarson1671@gmail.com>
2017-08-30 11:39:52 -0400

    working example

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const models = require('./models');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const sequelize = require('sequelize');
+
+//app
+const app = express();
+//views
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
-//FILTER BY CRITERIA
-models.User.findAll({
-    where: {bio: ''}
-}).then((results) => {
-  results.forEach((result) => {
-    console.log("================");
-    console.log(result.name);
-    console.log(result.email);
-    console.log(result.bio);
-  })
+
+
+app.get('/', (req, res) => {
+  models.User.findAll(
+    //below, ordering by the updatedAt col (required in sequelize)
+    {order: sequelize.col('updatedAt')}
+  ).then((results) =>{
+    res.render('index', {model: results});
+  });
 });
 
+app.get('/users/:id', (req, res) => {
+  models.User.findById(req.params.id).then((result) => {
+    res.render('detail', {model: result});
+  });
+});
 
-
-
-//ADDED USERS TO DATABASE
-
-// models.User.create({
-//   name: 'Babadook',
-//   email: 'spooky@scary.org',
-//   bio: 'gimmme da book'
-// });
-//
-// models.User.create({
-//   name: 'vegeta',
-//   email: 'saiyanprince@google.com',
-//   bio: 'KAKAROT!!!'
-// });
+app.listen(3000);
